218f4b67b5f445d5b91c5c0570b1013ee495da0c
shree <shree@LAPTOP-2QEA4LIA>
2024-01-11 11:31:19 +0530

    By saurabh

---
+++ Git_Demo/src/com/cjc/demo/AA.java
@@ -0,0 +1,41 @@
+package com.cjc.demo;
+
+public class AA {
+	
+	
+	
+	public void m1()
+	{
+		System.out.println("M1");
+	}
+	
+	public void m2()
+	{
+		System.out.println("M2");
+	}
+	
+	public void m3()
+	{
+		System.out.println("M3");
+	}
+	
+	public void m4()
+	{
+		System.out.println("M4");
+	}
+	
+	public void m5()
+	{
+		System.out.println("M5");
+	}
+
+public static void main(String[] args) {
+		
+		AA aa=new AA();
+		aa.m1();
+		aa.m2();
+		aa.m3();
+		aa.m4();
+		aa.m4();
+	}	
+}
