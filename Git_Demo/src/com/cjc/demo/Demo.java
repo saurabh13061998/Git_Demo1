3ceeddd76f6af41b099c6dc972650d47c17217f0
shree <shree@LAPTOP-2QEA4LIA>
2024-01-11 11:15:35 +0530

    By saurabh

--- Git_Demo/src/com/cjc/demo/Demo.java
+++ Git_Demo/src/com/cjc/demo/Demo.java
@@ -5,5 +5,12 @@ public class Demo
 	public static void main(String[] args) {
 		System.out.println("HELLO GIT");
 	}
+	
+	public void m1()
+	{
+		System.out.println(" Testing");
+		System.out.println(" GitHub ");
+		
+	}
 
 }
